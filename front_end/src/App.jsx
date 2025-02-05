b19bf1666f7aadf53ed312d6623db5188d246991
Kiran Raj <kiranrajb5882@gmail.com>
2025-02-05 23:26:01 +0530

    add

---
+++ front_end/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <h1 className="text-3xl font-bold underline text-red-950">
+    Hello world!
+  </h1>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React application.</p>
+      <p>Welcome to the <strong>React Tutorial</strong> series.</p>
+      <p>You can find the code on <a href="https://github.com/reactjs/reactjs.org/tree/main/content/tutorial/tutorial.md">GitHub</a>.</p>
+      <p>Enjoy your learning journey!</p>
+      
+    </div>
+  )
+}
+
+export default App
