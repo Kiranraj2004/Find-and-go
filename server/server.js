decfc97d5a83ff1e121a5efba541ac7532abffdc
Kiran Raj <kiranrajb5882@gmail.com>
2025-02-26 16:34:24 +0530

    add

--- server/server.js
+++ server/server.js
@@ -18,6 +18,18 @@ app.use(express.json()); // Properly parse JSON requests
 // Connect to MongoDB
 connectDB();
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://find-and-go-2.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Preflight request
+  }
+  next();
+});
+
 app.use((req, res, next) => {
     console.log(`Incoming ${req.method} request to ${req.url}`);
     next();
